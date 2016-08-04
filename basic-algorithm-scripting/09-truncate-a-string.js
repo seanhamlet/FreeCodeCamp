772d4d01ae550ab2e7f28921962bcf835354c594
Sean M Hamlet <seanmhamlet@gmail.com>
2016-08-04 14:45:58 -0400

    Initial commit of truncate-a-string

---
+++ basic-algorithm-scripting/09-truncate-a-string.js
@@ -0,0 +1,18 @@
+/* Basic Algorithm: Truncate a String
+
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+Note that inserting the three dots to the end will add to the string length.
+
+However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful hints:
+
+String.prototype.slice()
+
+Written by: Sean M Hamlet
+https://www.freecodecamp.com/seanmhamlet
+*/
+
