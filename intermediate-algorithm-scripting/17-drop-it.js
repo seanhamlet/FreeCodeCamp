544c29193a8d7d847a28b6f8f297edb8b2158cf7
Sean M Hamlet <seanmhamlet@gmail.com>
2016-08-06 08:46:02 -0400

    Initial commit of drop-it

---
+++ intermediate-algorithm-scripting/17-drop-it.js
@@ -0,0 +1,22 @@
+/* Intermediate Algorithm: Drop It
+
+Drop the elements of an array (first argument), starting from the front, until 
+the predicate (second argument) returns true.
+
+The second argument, func, is a function you'll use to test the first elements 
+of the array to decide if you should drop it or not.
+
+Return the rest of the array, otherwise return an empty array.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Arguments object
+Array.prototype.shift()
+Array.prototype.slice()
+
+Written by: Sean M Hamlet
+https://www.freecodecamp.com/seanmhamlet
+*/
+
